9c8243988bac4481d8b5f1843a43995665bd173b
Amal Sindhu Sasidharan <amal0109@gmail.com>
2025-06-18 12:16:12 +0530

    using fetch call for user data using component state

--- src/pages/UserPage.jsx
+++ src/pages/UserPage.jsx
@@ -1,7 +1,32 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 const UserPage = () => {
-  return <div>User info</div>;
+  const [userData, setUserData] = useState(null);
+
+  useEffect(() => {
+    const returnUserData = async () => {
+      const response = await fetch("https://fakestoreapi.com/users/1");
+      const result = await response.json();
+      setUserData(result);
+    };
+    returnUserData();
+  }, []);
+
+  return (
+    <div>
+      <h1>User Info</h1>
+      {userData && (
+        <>
+          Name: {userData.name.firstname} {userData.name.lastname} <br />
+          UserName : {userData.username} <br />
+          EmailId: {userData.email}
+          <br />
+          Address : {userData.address.number}, {userData.address.street},{" "}
+          {userData.address.city}, {userData.address.zipcode}
+        </>
+      )}
+    </div>
+  );
 };
 
 export default UserPage;
